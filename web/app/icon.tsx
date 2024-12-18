35f94cf4c6b104e0e599426b6bf67092b34b674e
W. Kyle Graves <williamkylegraves@gmail.com>
2024-12-17 20:46:49 -0500

    Fix favicon radius

--- web/app/icon.tsx
+++ web/app/icon.tsx
@@ -33,6 +33,7 @@ export default function Icon() {
             width: "100%",
             height: "100%",
             objectFit: "cover",
+            borderRadius: "50%",
           }}
         />
       </div>
