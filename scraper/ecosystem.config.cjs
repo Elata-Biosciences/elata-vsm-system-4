54b41f799242ad65fbc8cb93c8f5b48ca55c7955
W. Kyle Graves <williamkylegraves@gmail.com>
2025-01-29 00:12:40 -0500

    Remove old pm2 server

--- scraper/ecosystem.config.cjs
+++ scraper/ecosystem.config.cjs
@@ -1,43 +1,23 @@
 module.exports = {
-    apps: [
-        {
-            name: "elata-newsbot-scraper",
-            script: "./dist/index.js",
-            watch: false,
-            instances: 1,
-            autorestart: false,
-            cron_restart: "0 12 * * *",
-            max_memory_restart: '1G',
-            env_file: ".env",
-            env: {
-                NODE_ENV: 'development'
-            },
-            env_production: {
-                NODE_ENV: 'production'
-            },
-            cwd: "./",
-            interpreter: "node",
-            interpreter_args: "--experimental-specifier-resolution=node"
-        },
-        {
-            name: "elata-newsbot-server",
-            script: "./dist/server.js",
-            watch: true,
-            instances: 1,
-            autorestart: true,
-            max_memory_restart: '500M',
-            env_file: ".env",
-            env: {
-                NODE_ENV: 'development',
-                PORT: 2345
-            },
-            env_production: {
-                NODE_ENV: 'production',
-                PORT: 2345
-            },
-            cwd: "./",
-            interpreter: "node",
-            interpreter_args: "--experimental-specifier-resolution=node"
-        }
-    ]
-}
+  apps: [
+    {
+      name: "elata-newsbot-scraper",
+      script: "./dist/index.js",
+      watch: false,
+      instances: 1,
+      autorestart: false,
+      cron_restart: "0 12 * * *",
+      max_memory_restart: "1G",
+      env_file: ".env",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+      cwd: "./",
+      interpreter: "node",
+      interpreter_args: "--experimental-specifier-resolution=node",
+    },
+  ],
+};
